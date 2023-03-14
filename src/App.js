02518f5ff8db0d3603c7543817db301891b3cd39
Stephen Doty <stephendoty826@yahoo.com>
2023-03-14 18:48:52 -0500

    reviewing React and how to use useState, onClick, and onChange

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Button from 'react-bootstrap/Button';
 
-function App() {
+const App = () => {
+
+  const [number, setNumber] = useState(0)
+  const [count, setCount] = useState(0)
+
+  const incrementCountBy = (num) => {
+    let number = parseInt(num)
+    setCount(count + number)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div>Count: {count}</div>
+      Increment Count by:
+      <input onChange={(e) => setNumber(e.target.value)}></input>
+      <br/>
+      <Button onClick={() => incrementCountBy(number)} >Increment</Button>
+      <Button onClick={() => setCount(0)} variant="secondary">Reset</Button>
+
+      <Button onClick={() => setCount(count - 1)} variant="danger">Decrement</Button>
+    </>
+    
+  )
 }
 
-export default App;
+export default App
